17d32af97f41145325d1810fa81823ac44c98e85
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2021-07-03 02:04:08 +0430

    debug

--- core/spotify-agent/index.js
+++ core/spotify-agent/index.js
@@ -1,34 +1,22 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const { clientId, clientSecret } = require('../config');
+const { getDevices } = require('./requests')
 const store = require('./store')
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret,
-  redirectUri: 'http://raspberrypi.local:3000/callback'
-});
 
 const setAccessToken = (token) => {
   store.setState({ accessToken: token })
-  spotifyApi.setAccessToken(token)
   setup()
-}
-
-const getCurrentPlayingTrack = () => {
-  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-    function(data) {
-      console.log('Artist albums', data.body);
-    },
-    function(err) {
-      console.error(err);
-    }
-  );
 }
 
 const setup = () => {
   getCurrentPlayingTrack()
 }
 
+const getCurrentPlayingTrack = async () => {
+  console.log(await getDevices());
+}
+
+if (store.state.accessToken) {
+  setAccessToken(store.state.accessToken)
+}
 
 module.exports = {
   setAccessToken
---
+++ core/spotify-agent/requests.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const store = require('./store')
+
+const call = async (url = '', { method = 'GET' }) => {
+  const options = {
+    method,
+    url: `https://api.spotify.com/v1/${url}`,
+    headers: {
+      'Authorization': `Bearer ${store.state.accessToken}`,
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    }
+  }
+  return await axios(options);
+}
+
+const getDevices = async () => {
+  return await call('player/devices')
+}
+
+module.exports = {
+  call,
+  getDevices
+}
